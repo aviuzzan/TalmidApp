d2810e139b88a740a017e59ec2fe9157bf248f6e
aviuzzan <avi.uzzan@loubavitch.fr>
2026-05-04 17:23:24 +0200

    Add files via upload

--- src/app/admin/ecoles/[id]/layout.tsx
+++ src/app/admin/ecoles/[id]/layout.tsx
@@ -1,4 +1,3 @@
-import AdminLayout from '@/components/ui/AdminLayout'
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <AdminLayout>{children}</AdminLayout>
+  return <>{children}</>
 }
