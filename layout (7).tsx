7e94160dd51d8de89d3c5ab6a42274495d062910
aviuzzan <avi.uzzan@loubavitch.fr>
2026-05-06 16:26:06 +0200

    Delete layout (7).tsx

--- layout (7).tsx
+++
@@ -1,4 +0,0 @@
-import AdminLayout from '@/components/ui/AdminLayout'
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <AdminLayout>{children}</AdminLayout>
-}
